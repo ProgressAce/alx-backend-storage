e1d8378098c7be639629f7c8b61f3fabe6556eba
ProgressAce <daiyaansallie7@gmail.com>
2024-01-26 14:53:00 +0000

    add tests for exercise 3

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -6,6 +6,8 @@ import redis
 
 Cache = __import__('exercise').Cache
 
+
+print("\n----Exercise 1----\n")
 cache = Cache()
 
 data = b"hello"
@@ -14,3 +16,48 @@ print(key)
 
 local_redis = redis.Redis()
 print(local_redis.get(key))
+
+del cache
+
+
+print("\n----Exercise 2----\n")
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    print(fn)
+    assert cache.get(key, fn=fn) == value
+assert cache.get('yoyo') == None  # key does not exist
+
+k1 = cache.store("yo")
+assert cache.get_str(k1) == "yo"
+assert cache.get_str("cabbage") == None  # key does not exist
+
+k2 = cache.store("8")
+assert cache.get_int(k2) == 8
+assert cache.get_int("cabbage") == None  # key does not exist
+
+del cache
+
+
+print("\n----Exercise 3----\n")
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
+
+del cache
+
+
+print("\n----Exercise 4----\n")
+cache = Cache()
