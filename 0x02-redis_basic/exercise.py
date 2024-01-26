bffd432326b44dbdb3214f611b2acc856a49b61a
ProgressAce <daiyaansallie7@gmail.com>
2024-01-26 13:37:17 +0000

    change store method's type annotation to include bytes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,22 @@
 
 import redis
 import uuid
-from typing import Union, ByteString
+from typing import Any, Callable, Optional, Union
 
+
+def count_calls(self, method: Callable[[None], None]) -> Callable[[None], None]:
+    """Counts the number of times a method was called."""
+    key = method.__qualname__
+    self._redis.set(key, 0)  # key for counting a method's call count
+
+    @functools.wraps(method)
+    def wrapper(*args):
+        """Performs the count incrementally."""
+        self._redis.incr(key)
+
+        return method(args)
+
+    return wrapper
 
 class Cache:
     """Represents cache system using Redis."""
@@ -15,7 +29,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, ByteString, int, float]) -> str:
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key and persists the <data> into Redis.
 
         Arg:
@@ -28,3 +43,52 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable[[ByteString], Any]]=None) -> Any:
+        """Gets the value of a key in its desired Python format.
+
+        Args:
+            key: the key to look for in Redis.
+            fn: a function used to convert the found value to its
+            desirable format.
+
+        Returns:
+            The value in its desirable format, instead of as a byte.
+            Same with Redis, if key not found then None is returned."""
+
+        value = self._redis.get(key)
+        if value is None:
+            return None
+
+        if fn is None:
+            return value
+
+        desired_value = fn(value)
+        return desired_value
+
+    def get_str(self, key: str) -> str:
+        """Provides <Cache.get> with the correct function for str conversion.
+
+        Args:
+            key: the key to pass to <self.get>
+
+        Returns:
+            a str value from <self.get>
+        """
+
+        value = self.get(key, lambda val: val.decode("utf-8"))
+        return value
+
+    def get_int(self, key: str) -> int:
+        """Provides <Cache.get> with the correct function for int conversion.
+
+        Arg:
+            key: the key to pass to <self.get>.
+
+        Returns:
+            an int value from <self.get>
+        """
+
+        value = self.get(key, int)
+        return value
